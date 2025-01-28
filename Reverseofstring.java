15558128075bb849be20e1f61ca10ecc2b71a2a4
CHINNAM HANSIKA PATEL <hansikachinnam12@gmail.com>
2025-01-28 16:05:11 +0530

    Add files via upload

---
+++ Reverseofstring.java
@@ -0,0 +1,18 @@
+public class Reverseofstring {
+
+        public static  void main(String[] args){
+            String str="My Name is Chithra";
+            String reversed = reverseString(str);
+            System.out.println("The reversed string is:"+reversed);
+        }
+    
+        public static String reverseString(String str)
+        {
+    
+            if(str.isEmpty())
+            return str;
+            return reverseString(str.substring(1))+str.charAt(0);
+        }
+    
+    }
+
